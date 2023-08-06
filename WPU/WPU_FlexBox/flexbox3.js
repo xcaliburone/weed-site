c7bc6e98454bec09445b4aa3a3bd74573577b729
adhimrahman <adhimrahaman@gmail.com>
2023-08-06 17:42:53 +0800

    script

--- WPU/WPU_FlexBox/flexbox3.js
+++ WPU/WPU_FlexBox/flexbox3.js
@@ -4,3 +4,16 @@ const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click', () => {
     nav.classList.toggle('slide');
 })
+
+$('.page-scroll').on('click', function(e) {
+
+    var tujuan = $(this).attr('href');
+   
+    var elemenTujuan = $(tujuan);
+   
+    $('html , body').animate({
+     scrollTop: elemenTujuan.offset().top
+    }, 1250, 'linear' );
+   
+    e.preventDefault();
+});
