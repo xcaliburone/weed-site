456f4187c783fa644defcfccc4ea9e121b06c179
adhimrahman <adhimrahaman@gmail.com>
2023-12-19 18:07:11 +0800

    delete

--- IDCamp2023/Belajar Dasar Pemrograman Web/Final/script.js
+++
@@ -1,5 +0,0 @@
-// Terdapat penerapan JavaScript dalam memanipulasi DOM
-//         - Membuat dropdown.
-//         - Memanfaatkan logika seperti looping dalam menampilkan elemen dan konten.
-//         - Membuat slider.
-//         - Jika submission Anda terdapat penerapan JS, tuliskan feature yang dibuat dan jelaskan cara mengakses pada Student's Notes.
